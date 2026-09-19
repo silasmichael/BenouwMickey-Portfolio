9992592b4f1520799132781c4c1596b09e4f968d
Michael Silas <32719824+silasmichael@users.noreply.github.com>
2026-09-19 08:32:07 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// Supabase connection settings (both values are safe to be public).
+const CONFIG = {
+  SUPABASE_URL: 'https://gfwrkjohchppejaueckk.supabase.co',
+  SUPABASE_KEY: 'sb_publishable_IWfeM-zWkV45p8XPHVD3AA_nWv-t_F0'
+};
